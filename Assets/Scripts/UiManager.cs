7c7e8a23b00f36ecf6dce6953be420485c5a0cac
DESKTOP-MPLH4SN\Gonca <goncasilik@gmail.com>
2020-09-30 02:02:26 +0300

    Add progress bar

---
+++ Assets/Scripts/UiManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+
+public class UiManager : MonoBehaviour
+{
+    public static UiManager instance;
+
+    // Level Progress UI
+    [SerializeField] int sceneOffset;
+    [SerializeField] TMP_Text nextLevelText;
+    [SerializeField] TMP_Text currentLevelText;
+    [SerializeField] Image progressFillImage;
+
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    private void Start()
+    {
+        progressFillImage.fillAmount = 0f;
+        SetLevelProgressText();
+    }
+
+    void SetLevelProgressText()
+    {
+        int level = SceneManager.GetActiveScene().buildIndex + sceneOffset;
+        currentLevelText.text = (level).ToString();
+        nextLevelText.text = (level + 1).ToString();
+    }
+
+    // Update is called once per frame
+    public void UpdateLevelProgress()
+    {
+        float val = 1f - ((float)Level.instance.objectsInScene / Level.instance.totalObjects);
+        progressFillImage.fillAmount = val;
+    }
+}
