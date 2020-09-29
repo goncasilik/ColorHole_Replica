245d9ac0f56e08b108bc74a3a93f8756b99e1f5e
DESKTOP-MPLH4SN\Gonca <goncasilik@gmail.com>
2020-09-30 01:13:24 +0300

    Add objects and obstacles

---
+++ Assets/Scripts/UndergroundCollision.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UndergroundCollision : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        string tag = other.tag;
+
+        if(tag.Equals("Object"))
+        {
+            Debug.Log("Object");
+        }
+        if(tag.Equals("Obstacle"))
+        {
+            Debug.Log("Obstacle");
+
+            //restart level
+        }
+    }
+}
