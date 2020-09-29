0b1a098e7be40dba44c6a8dd07d2108ee0e71b52
DESKTOP-MPLH4SN\Gonca <goncasilik@gmail.com>
2020-09-29 23:18:28 +0300

    Implement hole movement

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game
+{
+    public static bool isGameover = false;
+    public static bool isMoving = false;
+}
